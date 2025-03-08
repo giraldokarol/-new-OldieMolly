70aec4f7399224f9818ddd83836f7c08482115b8
Karol Giraldo Escobar <karol.giraldo9505@gmail.com>
2025-03-08 22:17:35 +0100

    feat: Add mixing for show icon in svg

--- src/components/Header_Home.vue
+++ src/components/Header_Home.vue
@@ -1,14 +1,11 @@
 <script setup lang="ts">
-
 const {info} = defineProps(["info"]);
-
-
 </script>
 
 <template>
     <div class="om_bigtitle_container">
-        <div class="om_big_title_image">
-            <span class="bee_icon icon_large" 
+        <div class="om_bigtitle_image">
+            <span class="om_icon_bee icon_large" 
             :class="{'om_yllback_def': info.name == 'Login' , 'om_orangback_def':info.name == 'Sign Up'}"
             aria-hidden="true"></span>
         </div>
@@ -16,3 +13,17 @@ const {info} = defineProps(["info"]);
         <p class="om_gigtitle_description">{{ info.description }}</p>
     </div>
 </template>
+
+<style scoped lang="scss">
+.om_bigtitle {
+    &_image{
+        @include rem(margin-bottom, 30);
+        span[class^="om_icon"]{
+            align-items: center;
+            justify-content: center;
+            @include svgSize(52);
+            @include svgColor($om_orange_color1);
+        }
+    }
+}
+</style>
